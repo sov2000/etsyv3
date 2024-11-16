11f36e51ca2fa30ac5dacdc9b286ce1c1bee990a
sov2000 <stasovcharenko@gmail.com>
2024-11-16 00:35:05 -0500

    added static method to gen draft listing request from response

--- etsyv3/models/__init__.py
+++ etsyv3/models/__init__.py
@@ -1,11 +1,12 @@
 from .listing_inventory import ListingInventory
 from .listing_property import ListingProperty
-from .listing_request import Request, UpdateListingRequest, UpdateVariationImagesRequest, UpdateListingInventoryRequest
+from .listing_request import Request, CreateDraftListingRequest, UpdateListingRequest, UpdateVariationImagesRequest, UpdateListingInventoryRequest
 
 __all__ = [
     "ListingInventory",
     "ListingProperty",
     "Request",
+    "CreateDraftListingRequest",
     "UpdateListingRequest",
     "UpdateVariationImagesRequest",
     "UpdateListingInventoryRequest"
